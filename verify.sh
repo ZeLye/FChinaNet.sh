2610b44fb33d70c299f245c59d0f3ca8674c7d47
Anapopo <favoursmoe@gmail.com>
2018-04-08 16:14:31 +0800

    naive done it

--- verify.sh
+++ verify.sh
@@ -1,25 +1,46 @@
 #! /bin/bash
 
-AUTH=""
-# if AUTH == "" ; then
-#  return
-# fi
+AUTH=":"
+if [ "x$AUTH" == "x:" ] ; then
+  echo "[!] 请按照AUTH=\"user:pass\"格式填入掌上大学账户和密码！"
+  exit
+fi
 
 UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36"
 
-# user_id
+# 获取用户ID
 ID=$(curl -sS "https://www.loocha.com.cn:8443/login/" \
   --user $AUTH \
   --user-agent "$UA" \
   | grep -Eo '\"id\":\"[0-9]{7}\"' | awk -F: '{print $2}' | sed 's/"//g')
-echo "user id:" $ID
+echo "id: " $ID
 
-# CODE
+# 获取随机密码CODE
 CODE=$(curl -sS "https://wifi.loocha.cn/${ID}/wifi/telecom/pwd?type=4" \
   --user $AUTH \
+  --user-agent "$UA" \
   | grep -Eo '\"password\":\"[0-9]{6}\"' | awk -F: '{print $2}' | sed 's/"//g')
 
-echo "code: "$CODE
+echo "code: " $CODE
 
-# wan_ip/bras_ip
-# curl -sSI "http://test.f-young.cn/"
+# 获取 WAN_IP 和 BRAS_IP
+LOCATION=$(curl http://test.f-young.cn -sSI | grep -E 'Location')
+WAN_IP=$(echo $LOCATION | awk -F '[:?&]' '{print $4}' | awk -F= '{print $2}')
+BRAS_IP=$(echo $LOCATION | awk -F '[:?&]' '{print $5}'| awk -F= '{print $2}')
+
+echo "wan_ip: " $WAN_IP
+echo "bras_ip: " $BRAS_IP
+
+# 获取 QRCODE
+QRCODE=$(curl -sS "https://wifi.loocha.cn/0/wifi/qrcode?brasip=${BRAS_IP}&ulanip=${WAN_IP}&wlanip=${WAN_IP}" \
+  | grep -Eo 'HIWF://[a-z0-9]{32}')
+
+echo "qrcode: " $QRCODE
+
+# 开始登录
+PARAM="qrcode=${QRCODE}&code=${CODE}&type=1"
+
+curl -X POST \
+     --user $AUTH \
+     --user-agent "$UA" \
+     "https://wifi.loocha.cn/${ID}/wifi/telecom/auto/login?${PARAM}"
