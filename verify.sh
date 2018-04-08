25b7c72e859d41b69a2efb55e76e7932bc0f2392
Anapopo <favoursmoe@gmail.com>
2018-04-08 13:07:26 +0800

    Add UA

--- verify.sh
+++ verify.sh
@@ -2,4 +2,4 @@
 AUTH=""
 curl "https://www.loocha.com.cn:8443/login" \
   --user $AUTH \
-  --user-agent ""
+  --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36"
