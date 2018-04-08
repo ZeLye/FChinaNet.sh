d918e05cdd9f819c3e2d9beee89c16094979e9fd
Anapopo <rasuno@163.com>
2018-04-08 15:24:52 +0800

    Done user_id/Code

--- verify.sh
+++ verify.sh
@@ -1,5 +1,25 @@
 #! /bin/bash
+
 AUTH=""
-curl "https://www.loocha.com.cn:8443/login" \
+# if AUTH == "" ; then
+#  return
+# fi
+
+UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36"
+
+# user_id
+ID=$(curl -sS "https://www.loocha.com.cn:8443/login/" \
   --user $AUTH \
-  --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36"
+  --user-agent "$UA" \
+  | grep -Eo '\"id\":\"[0-9]{7}\"' | awk -F: '{print $2}' | sed 's/"//g')
+echo "user id:" $ID
+
+# CODE
+CODE=$(curl -sS "https://wifi.loocha.cn/${ID}/wifi/telecom/pwd?type=4" \
+  --user $AUTH \
+  | grep -Eo '\"password\":\"[0-9]{6}\"' | awk -F: '{print $2}' | sed 's/"//g')
+
+echo "code: "$CODE
+
+# wan_ip/bras_ip
+# curl -sSI "http://test.f-young.cn/"
